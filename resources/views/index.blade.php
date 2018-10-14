e99c2752a117f1274437164a835d245995915d31
Julian <julianpitre1997@gmail.com>
2018-10-14 13:01:53 -0500

    avance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,18 @@
 @section('controller', 'ng-controller="indexController"')
 @section('content')
 
-
+<div class="container mt-2">
+	<form class="form-inline">
+		<div class="form-group mr-3">
+			<label class="mr-2">Año</label>
+			<select class="form-control"></select>
+		</div>
+		<div class="form-group mr-3">
+			<label class="mr-2">Grado</label>
+			<select class="form-control"></select>
+		</div>
+	</form>
+</div>
+	
 
 @stop
