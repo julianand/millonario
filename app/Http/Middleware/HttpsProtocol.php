fcb16086e68e88b7659106499cf635ed778c328e
Julian <julianpitre1997@gmail.com>
2018-10-24 08:11:29 -0500

    q

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,6 +16,7 @@ class HttpsProtocol
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && \App::environment() === 'production') {
+            $request->setTrustedProxies( [ $request->getClientIp() ] ); 
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
