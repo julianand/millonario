d0cd7c49ccad8e82252f5090e642c8e5dc9a47bf
Julian <julianpitre1997@gmail.com>
2018-10-23 23:32:15 -0500

    a

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,6 +21,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.5/angular.min.js"></script>
-    <script type="text/javascript" src="{{secure_asset('js/juego.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/juego.js')}}"></script>
   </body>
 </html>
